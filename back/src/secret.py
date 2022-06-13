48298594b72c824b496e9ea36e1e631374cc887c
dcase808 <xmarcinx125@gmail.com>
2022-06-13 23:21:49 +0200

    add Procfile

--- back/src/secret.py
+++ back/src/secret.py
@@ -1,2 +1,2 @@
-CONNECTION_STRING = 'XXX'
-SECRET_KEY = 'XXX'
+CONNECTION_STRING = 'mongodb+srv://mongo:BhOeUo5x5i9yHvhb@cinemative0.zfdcu.mongodb.net/Cinemative0?retryWrites=true&w=majority'
+SECRET_KEY = '2a4c99b08c99d69f113d859c7d8c091f01e572e8e98826b161ca00f23260453d'
